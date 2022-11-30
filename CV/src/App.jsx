59d715a2c457cdd2a28d467a14cf72fcd4d8b1a2
HuseyNN021 <hhsnov880@gmail.com>
2022-12-01 03:25:38 +0400

    React

---
+++ CV/src/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const H1=()=>{
+return(
+  <>
+    <h1>Salam</h1>
+  </>
+  )
+}
+
+export default H1
+
+// import React, { Fragment } from 'react'
+// import './index.css'
+// const H1=()=>{
+// return(
+//   <>    
+//     <div className='DS'>
+//       <div>
+//       <img src="https://img.freepik.com/free-photo/abstract-grunge-decorative-relief-navy-blue-stucco-wall-texture-wide-angle-rough-colored-background_1258-28311.jpg?w=1800&t=st=1669848508~exp=1669849108~hmac=c71bc5c82f0db7564d1c880de9047dd38686d07e3eb6c0e78cc69f3e0673d8d6" alt="M" width="200px" />
+//       </div>
+//       <div className='info'>
+//       <p>Ad ve soyad:</p>
+//       <p>Huseyn Hesenov</p>
+//       <p>Yas:</p>
+//       <p>19</p>
+//       <p>Unvan:</p>
+//       <p>Nesimi rayonu,9cu mikrorayon,157 sayli mektebin yani</p>
+//       </div>
+//     </div>
+//   </>
+//   )
+// }
+
+// export default H1
+
+
+// .DS{
+//   display: flex;
+// }
+// img{
+//   margin-right: 10px;
+//   width: 100px;
+//   height: 100px;
+// }
+// .info{
+//   border: 1px solid red;
+//   padding: 5px;
+//   border-radius: 10px;
+//   background-color: aqua;
+// }
+// p{
+//   border: 1px solid black;
+//   padding: 5px;
+//   border-radius: 10px;
+//   background-color: white;
+// }
---
+++ CV/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+import H1 from './App'
+console.log("salam")
+const element = (
+  
+  <>
+    <h1>Hello!</h1>
+    <p>Welcome to React!</p>
+    <img src="" alt="" srcset="" />
+    <H1/>
+  </>
+);
+
+// import React from "react"; 
+// import ReactDOM from "react-dom"; 
+// import App from './PersonalInfo'
+// const element = (
+//   <>
+//     <App/>
+//   </>
+// );
+
+// const rootElement = document.querySelector("#root");
+
+// ReactDOM.render(element, rootElement);
